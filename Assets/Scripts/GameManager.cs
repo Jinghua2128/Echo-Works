d27ee85c20f712b99105e7294446eec1dc19f55f
Liu GuangXuan <s10268212@connect.np.edu.sg>
2026-05-13 10:52:58 +0800

    Init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject("GameManager");
+                    instance = obj.AddComponent<GameManager>();
+                }
+            }
+            return instance;
+        }
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
